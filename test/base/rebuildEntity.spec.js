2e9219006ac9cf6372d75aa95e97d30527b125cf
Kai Curry <kai@cape.io>
2016-08-30 19:37:51 -0500

    more making sure rebuildEntity works

--- test/base/rebuildEntity.spec.js
+++ test/base/rebuildEntity.spec.js
@@ -2,11 +2,10 @@ import test from 'tape'
 import { combineReducers, createStore } from 'redux'
 import { get } from 'lodash'
 
-import graph, { create, isEntity, rebuildEntitySelector } from '../../src'
-import { collection } from '../mock'
+import graph, { create, createTriple, isEntity, rebuildEntitySelector } from '../../src'
+import { collection, listItem } from '../mock'
 
-
-test('rebuildEntity()', t => {
+test('rebuildEntitySelector()', t => {
   const { dispatch, getState } = createStore(combineReducers({ graph }))
   create(dispatch, collection.creator)
   create(dispatch, collection.mainEntity)
@@ -14,13 +13,23 @@ test('rebuildEntity()', t => {
   const state = getState()
   // console.log(state)
   const subj = state.graph.entity[item.id]
+  t.deepEqual(item, subj)
   t.ok(isEntity(subj))
   t.notOk(subj.creator, 'creator')
   t.notOk(subj.mainEntity, 'mainEntity')
+  create(dispatch, listItem.item)
+  const triple = { subject: item, predicate: 'itemListElement', object: listItem }
+  const { object } = createTriple(dispatch, triple)
+  const state2 = getState()
   const buildEntity = rebuildEntitySelector(() => item.id)
-  const entity = buildEntity(state)
-  // console.log(entity)
+  const entity = buildEntity(state2)
+  // console.log(entity.itemListElement[object.id].item)
   t.ok(isEntity(get(entity, 'creator.user0')), 'creator')
   t.ok(isEntity(get(entity, 'mainEntity.pBlf')), 'mainEntity')
+  t.ok(isEntity(get(entity, [ 'itemListElement', object.id ])), 'itemListElement')
+  const id = listItem.item.id
+  const liItem = get(entity, [ 'itemListElement', object.id, 'item', id ])
+  t.ok(isEntity(liItem), 'itemListElement.item')
+  t.deepEqual(liItem, listItem.item, 'liItem')
   t.end()
 })
--- test/mock.js
+++ test/mock.js
@@ -10,18 +10,19 @@ addEntity('a1', 'bar')
 addEntity('b1', 'bar')
 addEntity('c1', 'bar')
 addEntity('d1', 'bar')
-
-export const agent = {
-  id: "user0",
-  type: "Person",
-  "name":"Anonymous",
-  "dateCreated":"2016-08-30T17:25:25.915Z",
+export const creator = {
+  id: 'user0',
+  type: 'Person',
+  name: 'Anonymous Person or User of the website',
 }
+export const agent = creator
+
 export const item = {
   approxWidth: '57"',
   category: 'textile',
   colors: [],
   contents: '100% Solution Dyed Acrylic',
+  dateCreated: new Date(),
   discontinued: false,
   id: '28',
   labelMessage: '5512',
@@ -34,25 +35,28 @@ export const item = {
   type: 'Item',
   isPattern: false,
 }
+export const listItem = {
+  actionStatus: 'created',
+  agent,
+  item,
+  position: 100,
+  startTime: '2016-08-30T17:41:43.233Z',
+  type: 'ListItem',
+}
 export const trouble = {
   subject: {
-    "additionalType":"Project","itemListOrder":"Ascending","title":"Favorites","type":"CollectionList","dateCreated":"2016-08-30T17:41:43.224Z","id":"i5dtxvjo"},
-  predicate: "itemListElement",
-  "object": {
-    actionStatus:"created",
-    agent,
-    item,
-    "position":100,
-    "startTime":"2016-08-30T17:41:43.233Z",
-    "type":"ListItem",
-  }
+    "additionalType":"Project",
+    "itemListOrder":"Ascending",
+    "title":"Favorites",
+    "type":"CollectionList",
+    "dateCreated":"2016-08-30T17:41:43.224Z",
+    "id":"i5dtxvjo",
+  },
+  predicate: 'itemListElement',
+  object: listItem,
 }
 export const mainEntity = { id: 'pBlf', type: 'DataFeed' }
-export const creator = {
-  id: 'user0',
-  type: 'Person',
-  name: 'Anonymous Person or User of the website',
-}
+
 export const title = 'Favorites'
 export const collection = {
   creator, // User that created the thing.
